3fed98d683e600aa071eda7ea84a129cc1c43ddb
Ashish Kumar Burnwal <ashishburnwal293@gmail.com>
2022-02-23 16:48:38 +0530

    small bug

--- src/index.js
+++ src/index.js
@@ -12,22 +12,28 @@ app.set("view engine", "hbs");
 app.use(express.static(staticPath));
 app.get("/", (req, res) => {
 	requests(
-		`https://api.openweathermap.org/data/2.5/weather?q=${req.query.name}&appid=e183db96cbba841ac570dfd40db56deb&units=metric&precipitation.value`
-	).on("data", (chunk) => {
-		const data = JSON.parse(chunk);
-		const myData = {
-			temp: data.main.temp,
-			iconUrl: iconUri + data.weather[0].icon + "@4x.png",
-			location: data.name,
-			country: data.sys.country,
-			mintemp: data.main.temp_min,
-			maxtemp: data.main.temp_max,
-			wind: data.wind.speed,
-		};
-		res.render("index", myData);
-	});
+		`https://api.openweathermap.org/data/2.5/weather?q=${
+			req.query.name || "dhanbad"
+		}&appid=e183db96cbba841ac570dfd40db56deb&units=metric&precipitation.value`
+	)
+		.on("data", (chunk) => {
+			const data = JSON.parse(chunk);
+			const myData = {
+				temp: data.main.temp,
+				iconUrl: iconUri + data.weather[0].icon + "@4x.png",
+				location: data.name,
+				country: data.sys.country,
+				mintemp: data.main.temp_min,
+				maxtemp: data.main.temp_max,
+				wind: data.wind.speed,
+			};
+			res.render("index", myData);
+		})
+		.on("error", (err) => {
+			console.log(err);
+		});
 });
 
-app.listen(port, (req, res) => {
+app.listen(port, () => {
 	console.log("listening on port " + port);
 });
